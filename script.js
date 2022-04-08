9032df2afd2251d75a5f67b98bccb403f4168561
dracosptifire <austin3flores@gmail.com>
2022-04-08 12:44:09 -0700

    Assignment Completed

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    
-    var interest = principal * years * rate /100;
-
-    var year = new Date().getFullYear()+parseInt(years);
-}
-function updateRate() 
-{
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_value").innerText=rateval;
-}
-function checkdata(){
+function compute() {
     //create references to the input elements we wish to validate
     var principal = document.getElementById("principal").value;
     //Check if principal field is empty
-    if(principal.value == ""){
+    if(principal == "") {
           alert("Please enter prinicpal amount over $0");
           principal.focus();
-          return false;
+          return;
     }
-    //If all is well return true.
-    alert("Form validation is successful.")
-    return true;
+    //declare values to be used
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear() + parseInt(years);
+    //display results
+    var result_display = document.getElementById("result");
+    result_display.innerText = "\n" +
+        "If you deposit $" + principal + "\n" +
+        "at an interest rate of " + rate + "%\n" +
+        "You will receive an amount of $" + interest + "\n" +
+        "in the year " + year;
+    
+}
+function updateRate() {
+    var rate_val = document.getElementById("rate").value;
+    //add percent symbol to value
+    var rate_per = rate_val + "%"
+    //display percent rate
+    var rate_update = document.getElementById("rate_value");
+    rate_update.innerText = rate_per;
+    //update slider value each time you drag the slider handle
+    document.getElementById("rate").oninput = function() {
+            rate_update.innerText = this.value + "%";
+        }
 }
